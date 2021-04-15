e3dc6aa1e3e9c9ac1d67ac1a5b1f2758af6a6065
Vimal Raveendran <vimalraveendra@gmail.com>
2021-04-15 23:02:09 +0200

    Optimized spacerComponent & also updated the RestaurantInfo component to render out Icon& isOpen element

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -1,7 +1,10 @@
 import React,{useContext} from 'react';
-import { Text} from 'react-native'
+import { Text,View,Image} from 'react-native'
 import {  Card} from 'react-native-paper';
 import styled from 'styled-components/native'
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star'
+import open from '../../../../assets/open'
 
 
 // here  we do styled(Card ) because Card is a component 
@@ -17,28 +20,87 @@ const RestaurantCardCover =styled(Card.Cover)`
  background-color:${props=>props.theme.colors.bg.primary}
 `;
 const TitleText =styled(Text)`
-    font-family:${props=>props.theme.fonts.body}; 
+    font-family:${props=>props.theme.fonts.heading}; 
     font-size:${props=>props.theme.fontSizes.body};
-    padding:${props=>props.theme.space[3]}; 
-    color:${props=>props.theme.colors.ui.error};
+    color:${props=>props.theme.colors.ui.primary};
 `;
+
+const Info =styled.View`
+  padding:${props=>props.theme.space[3]}; 
+`;
+
+const Address =styled.Text`
+   font-family:${props=>props.theme.fonts.body}; 
+   font-size:${props=>props.theme.fontSizes.caption};
+
+`
+const Section =styled.View`
+flex-direction:row;
+justify-content:space-between;
+align-items:center;
+
+`
+const Rating =styled.View`
+    flex-direction:row;
+    padding-top:${props=>props.theme.space[2]};
+    padding-bottom:${props=>props.theme.space[2]}
+`
+
+const SectionEnd=styled .View`
+  flex-direction:row;
+`;
+
+const Icon =styled.View`
+padding:0 16px
+`
 
 
 const RestaurantsInfoCard=({restaurant={}})=>{
   const { name = "Some Restaurant",
-  icon,
+  icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
   photos = [
     "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
   ],
   address = "100 some random street",
   isOpenNow = true,
   rating = 4,
-  isClosedTemporarily,} = restaurant;
+  isClosedTemporarily=true,} = restaurant;
+// here we creating an array of undefined items
+// Math.round is used to round down the value of rating to integer
+const ratingArray = Array.from(new Array(Math.round(rating)));
+
+
 return(
   
   <RestaurantCard elevation={5}>
     <RestaurantCardCover  key={name} source={{ uri:photos[0] }} />
-      <TitleText>{name}</TitleText> 
+     <Info>   
+      <TitleText>{name}</TitleText>
+      <Section>
+      <Rating>
+          {
+            ratingArray.map((item,index)=>(
+              <SvgXml width={20} height={20} xml={star} key={index} />
+              ))
+          }  
+       </Rating>
+       <SectionEnd>
+            {
+              isClosedTemporarily && (
+                <Text variant ='label' style={{color:'red',fontSize:16}}>ClosedTemporarily</Text>
+              )
+            }
+            <Icon>
+            {
+              isOpenNow&&<SvgXml width={20} height={20} xml={open} x/>
+            }
+            </Icon>
+            <Image style={{width:18,height:18}} source={{uri:icon}}/>
+      </SectionEnd>
+      </Section>
+      <Address>{address}</Address> 
+
+      </Info>
   </RestaurantCard>
 )
 }
