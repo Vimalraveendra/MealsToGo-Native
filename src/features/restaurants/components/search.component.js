c776ae1199390c22ed295a2c7572ccec81313c0a
Vimal Raveendran <vimalraveendra@gmail.com>
2021-04-27 12:04:40 +0200

    updated search Component& location Context to render the restaurantInfo component as expected

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,6 +1,5 @@
 import React,{useContext,useState,useEffect} from 'react';
 import {Searchbar} from 'react-native-paper'
-import { set } from 'react-native-reanimated';
 import styled from 'styled-components/native';
 import { LocationContext } from '../../../services/restaurants/location/location.context';
 
@@ -11,12 +10,7 @@ const Search = ()=>{
     const {keyword,search} = useContext(LocationContext) 
     const [searchKeyword,setSearchKeyword] = useState(keyword)
 
-    useEffect(() => {
-        search(searchKeyword)
-        // return () => {
-        //     cleanup
-        // }
-    }, [])
+   
     return (
         <SearchContainer >
           <Searchbar
