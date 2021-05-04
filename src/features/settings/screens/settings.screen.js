a50f6eb351c9951523321ead0398690f819374cd
Vimal Raveendran <vimalraveendra@gmail.com>
2021-05-04 23:03:50 +0200

    modified SettingsScreens to render the AvaterIcon

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,26 +1,42 @@
 import React,{useContext} from 'react';
-import {View,Text,Button} from 'react-native'
-import {List} from 'react-native-paper'
+import styled from  'styled-components/native'
+import {List,Avatar} from 'react-native-paper'
 
 import {AuthenticationContext} from '../../../services/authentication/authentication.context'
 
 import {SafeArea} from '../../../components/utility/safe-area.component'
+import {Text} from '../../../components/typography/text.component'
+import {Spacer} from '../../../components/spacer/spacer.component'
+
+
+const SettingsItem =styled(List.Item)`
+padding:${props=>props.theme.space[3]}; 
+`;
+
+const AvatarContainer =styled.View`
+align-items:center;
+`;
 
 export const SettingsScreen=({navigation})=> {
-    const{onLogout} = useContext(AuthenticationContext)
+    const{onLogout,isAuthenticated:user} = useContext(AuthenticationContext)
+   
      return (
        <SafeArea>
+       <AvatarContainer>
+       <Avatar.Icon size={180} icon="human"  backgroundColor='#2182BD'/>
+         <Spacer pos='top' size='large'>
+           <Text variant="label">{user.email}</Text>
+         </Spacer>
+       </AvatarContainer>
          <List.Section>
-            <List.Item
-            style={{padding:16}}
+            <SettingsItem
             title="Favorites"
             description="View Your Favorites"
             left={props=><List.Icon {...props} color="black" icon='heart'/> }
             onPress={()=>navigation.navigate('Favorites')}
             />
             
-           <List.Item
-             style={{padding:16}}
+           <SettingsItem
              title="Logout"
              left={props=><List.Icon {...props} color="black" icon='logout'/> }
              onPress={onLogout}
